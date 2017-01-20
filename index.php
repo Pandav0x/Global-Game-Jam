99dda5da91a5f558bceecb90b7949680bafd9bea
Daydreel <daydreel@gmail.com>
2017-01-20 22:38:41 +0100

    Modified index test

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>WIP Title</title>
+    <title>Project Plouf</title>
   </head>
   <body>
-    <h1>THIS is where the game will be</h1>
+    <h1>THIS is where the game will be and james AGREES</h1>
   </body>
 </html>
