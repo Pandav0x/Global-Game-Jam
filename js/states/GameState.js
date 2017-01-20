8a310d045f8ceccaba0c665804f115c2fe06fb62
Daydreel <daydreel@gmail.com>
2017-01-21 00:47:19 +0100

    touchPad modif

--- js/states/GameState.js
+++ js/states/GameState.js
@@ -10,8 +10,14 @@ GameState.prototype = {
 		console.log("preload Game");
 
 		Game.phaser_game.backgroundColor = "#4488AA";
+
+		var rectangle = new Game.phaser_game.Rectangle(100,200,600,200);
 	},
 	update: function(){
 
+	}
+
+	render: function(){
+		Game.phaser_game.debug(rectangle,'#0ffffff');
 	}
 }
