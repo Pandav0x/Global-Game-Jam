57350772966a052a7631881a0c47f386c664390c
Dehours Loïc <pandav0x@hotmail.fr>
2017-01-20 23:25:39 +0000

    Local Phaser (because CDN sucks in local without a network connection)

--- index.php
+++ index.php
@@ -3,21 +3,18 @@
   <head>
     <title>Project Plouf</title>
     <script src="https://code.jquery.com/jquery-3.1.1.js" integrity="sha256-16cdPddA6VdVInumRGo6IbivbERE8p7CQR3HzTBuELA=" crossorigin="anonymous"></script>
-    <script src="http://cdnjs.cloudflare.com/ajax/libs/phaser/1.1.3/phaser.min.js"></script>
+    <script src="./assets/libs/Phaser.js"></script>
 
     <!--the actual game-->
-    <script src="js/Game.js"></script>
     <script src="js/states/CreditState.js"></script>
     <script src="js/states/GameState.js"></script>
     <script src="js/states/LoseState.js"></script>
     <script src="js/states/MenuState.js"></script>
     <script src="js/states/PreloadState.js"></script>
     <script src="js/states/WinState.js"></script>
-
+    <script src="js/Game.js"></script>
   </head>
   <body>
-    <p> coucou c'est merlin </p>
-    <h1>THIS is where the game will be and james AGREES</h1>
-	<h1>You fail me for the last time Admiral !!!!</h1>
+    <div class="gameCanvas"></div>
   </body>
 </html>
--- js/Game.js
+++ js/Game.js
@@ -1,1 +1,29 @@
-console.log("Main game file");
+var Game = function(){
+    this.myId = 0;
+    this.phaser_game;
+
+    //Image path
+    this.sprite_folder = "./assets/images/spritesheet/";
+    this.texture_folder = "./assets/images/tile/";
+    this.image_folder = "./assets/images/";
+    this.buttons_folder = "./assets/images/buttons/";
+    this.screens_folder = "./assets/images/screens/";
+
+    //sound path
+    this.ambiance_folder = "./assets/sounds/";
+}
+
+Game.prototype.init = function(){
+    this.phaser_game = new Phaser.Game(800, 600, Phaser.AUTO, 'gameCanvas');
+
+    this.phaser_game.state.add("preloadState", PreloadState);
+    this.phaser_game.state.add("menuState", MenuState);
+    this.phaser_game.state.add("gameState", GameState);
+    this.phaser_game.state.add("winState", WinState);
+    this.phaser_game.state.add("loseState", LoseState);
+    this.phaser_game.state.add("creditState", CreditState);
+    this.phaser_game.state.start("preloadState");
+}
+
+var game = new Game();
+game.init();
