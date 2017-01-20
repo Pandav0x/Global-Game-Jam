45739294674a8ec6386d529ac76beeed00e9c1ed
Dehours Loïc <pandav0x@hotmail.fr>
2017-01-20 23:52:17 +0000

    Index: auto loader for libraries Jquery lib added to the project files

--- index.php
+++ index.php
@@ -2,20 +2,23 @@
 <html>
   <head>
     <title>Project Plouf</title>
-    <script src="https://code.jquery.com/jquery-3.1.1.js" integrity="sha256-16cdPddA6VdVInumRGo6IbivbERE8p7CQR3HzTBuELA=" crossorigin="anonymous"></script>
-    <script src="./assets/libs/Phaser.js"></script>
-
-    <!--the actual game-->
     <?php
-      $files = scandir("./js/states/");
-      foreach($files as $key => $value)
-        if(substr($value, 0, 1) != ".")
-          echo "<script src='js/states/$value'></script>";
+      importAll("./assets/libs"); //import all libraries
+      importAll("./js/states"); //import all game states
     ?>
-
     <script src="js/Game.js"></script>
   </head>
   <body>
     <div class="gameCanvas"></div>
   </body>
 </html>
+
+<?php
+  function importAll($path)
+  {
+    $files = scandir($path);
+    foreach($files as $key => $value)
+      if(substr($value, 0, 1) != ".")
+        echo "<script src='$path/$value'></script>";
+  }
+?>
