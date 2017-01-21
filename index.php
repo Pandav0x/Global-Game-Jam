2caecc67f2b0fd71c017b097821c088d0b8a8755
Dehours Loïc <pandav0x@hotmail.fr>
2017-01-21 04:00:14 +0000

    index: levels auto load creation of a folder for the levels

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
     <?php
       importAll("./assets/libs"); //import all libraries
       importAll("./js/states"); //import all game states
-      importAll("./js/models"); //import all game models
+      importAll("./js/models"); //import all game objects
+      importAll("./js/states/levles"); //import all game levels
     ?>
     <script src="js/Game.js"></script>
   </head>
--- js/models/TouchPad.js
+++ js/models/TouchPad.js
@@ -1,5 +1,4 @@
 var TouchPad = function(x,y){
-
     this.x = x;
     this.y = y;
     this.sprite = game.phaser_game.add.sprite(x,y,'touchpad');
@@ -14,18 +13,18 @@ var TouchPad = function(x,y){
 }
 
 TouchPad.prototype.update = function(){
-        
+
     if (game.phaser_game.time.now > this.nextFire)
     {
         if(this.sprite.input.pointerOver() && game.phaser_game.input.activePointer.leftButton.isDown){
             this.waveStrength++;
 
             console.log("down vague");
-        
+
             if(this.waveStrength <= 500)
                 this.waveStrength = 500;
 
-            this.nextFire = game.phaser_game.time.now + this.fireRate;       
+            this.nextFire = game.phaser_game.time.now + this.fireRate;
         }
     }
 }
