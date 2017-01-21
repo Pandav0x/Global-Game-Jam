7e1e33e28b2d738899010b6d9ed624c43afcd11c
Lilian Barreteau <lilian.barreteau@laposte.net>
2017-01-21 14:13:25 +0000

    Level 6-7-8-9

--- js/states/levels/LevelEightState.js
+++ js/states/levels/LevelEightState.js
@@ -14,6 +14,16 @@ LevelEightState.prototype = {
 		game.phaser_game.input.mouse.capture = true;
 
 		touchPad = new TouchPad(0,500);
+
+		var scaling = 0.15;
+		var redBoat = new RedBoat(170,100,scaling);
+		var redBoat = new RedBoat(500,200,scaling);
+		var redBoat = new RedBoat(250,300,scaling);
+
+		scaling = 0.3;
+		var mountain = new Mountain(150,150,scaling);
+		var mountain = new Mountain(480,250,scaling);
+		var mountain = new Mountain(230	,350,scaling);
 	},
 	update: function(){
 		touchPad.update();
--- js/states/levels/LevelSevenState.js
+++ js/states/levels/LevelSevenState.js
@@ -14,6 +14,17 @@ LevelSevenState.prototype = {
 		game.phaser_game.input.mouse.capture = true;
 
 		touchPad = new TouchPad(0,500);
+
+		var scaling = 0.15;
+
+		var rockHouse = new RockHouse(50,100,scaling);
+		var blueBoat = new BlueBoat(450,250,scaling);
+		var redBoat = new RedBoat(50,250,scaling);
+
+		scaling = 0.1;
+		var greenCorail = new GreenCorail(450,300,scaling);
+		var greenCorail = new GreenCorail(50,300,scaling);
+
 	},
 	update: function(){
 		touchPad.update();
